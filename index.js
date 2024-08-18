62df6bc2af9144ede7718788a87ee78d86a78523
AnnaMintel <hintell@proton.me>
2024-08-18 23:38:19 +0300

    lesson 5: working with files

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-//connecting modules
-const os = require('os');
-const res = os.platform();
-console.log(res);
+const fileSystem = require("fs");
 
-// connecting module my_math
-const my_math = require('./my_math');
-const math_add = my_math.add(3, 6);
-const math_mult = my_math.mult(2, 5);
-console.log(`Results: ${math_add} and ${math_mult}`);
+// reading info from the file
+fileSystem.readFile("some.txt", "utf-8", (err, data) => {
+  // create file and save data into
+  fileSystem.writeFile("some.txt", data + "\nSome text", (err, data) => {
+    console.log("This function is working!");
+  });
+});
