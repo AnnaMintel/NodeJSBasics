d57ccc8c10d56ecd5e25679f8af10e57fac5908d
AnnaMintel <hintell@proton.me>
2024-08-20 12:53:45 +0300

    lesson 6: creating and deleting files and folders

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
-const fileSystem = require("fs");
+const fs = require("fs");
 
-// reading info from the file
-fileSystem.readFile("some.txt", "utf-8", (err, data) => {
-  // create file and save data into
-  fileSystem.writeFile("some.txt", data + "\nSome text", (err, data) => {
-    console.log("This function is working!");
+//fs.mkdirSync('test_files');
+fs.mkdir('folder_1', () => {
+    fs.writeFile('./folder_1/aloha.txt', 'Hello', () => { })
+}) 
+
+// Create directory named 'text-files' and write a file in it
+fs.mkdir('folder_2', { recursive: true }, (err) => {
+    if (err) {
+        console.error('Error creating directory:', err);
+        return;
+    }
+    fs.writeFile('./folder_2/new.txt', 'Aloha', (err) => {
+        if (err) {
+            console.error('Error writing file:', err);
+        } else {
+            console.log('File created successfully!');
+        }
+    });
+});
+
+// creating folders with unique id
+const currentDate = new Date();
+const milliseconds = currentDate.getTime();
+
+fs.mkdir(`text-files-${milliseconds}`, () => {
+  fs.writeFile(`./text-files-${milliseconds}/new.txt`, "Hello", () => {
+    console.log("Success" + milliseconds);
   });
 });
+
+// deleting folders
+fs.unlink('./hello/hello.txt', () => {
+    fs.rmdir('./hello', () => {})
+})
