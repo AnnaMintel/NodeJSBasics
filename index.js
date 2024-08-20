ca1b4de8441b1be923d03e1cbd1ae703cc4049c7
AnnaMintel <hintell@proton.me>
2024-08-20 15:24:30 +0300

    lesson 8: URL tracking and HTML templates

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const http = require('http');
-const PORT = 3000;
+const fs = require('fs');
+
+const PORT = 3001;
 const HOST = 'localhost';
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain; charset=utf8'});
-    res.end('Hello NODE JS');
+    res.writeHead(200, {'Content-Type': 'text/html; charset=utf8'});
+
+    if(req.url == '/')
+        fs.createReadStream('./templates/index.html').pipe(res);
+    else if(req.url == '/about')
+        fs.createReadStream('./templates/about.html').pipe(res);
+    else
+    fs.createReadStream('./templates/error.html').pipe(res);
+    
 });
 
 server.listen(PORT, HOST, () => {
